a16e2691315146c5b36f5e5c26c81d920f09a31b
Carlos Saavedra <carlos.saavedrad@sansano.usm.cl>
2024-05-10 19:48:15 -0400

    Casos de prueba comentados

--- Codes/tests/test.py
+++ Codes/tests/test.py
@@ -4,10 +4,11 @@ import json
 import sqlite3
 
 class HistoryTests(unittest.TestCase):
+    #Clase de pruebas para el endpoint /historial
 
     @classmethod
     def setUpClass(cls) -> None:
-
+        #Para el setUp buscamos la información que existe en la base de datos.
         cls.connection = sqlite3.connect('talleres.db')
         cls.cursor = cls.connection.cursor()
         cls.url = "http://localhost:3000/historial"
@@ -20,17 +21,19 @@ class HistoryTests(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
+        #Para el tearDown cerramos la conexión con la base de datos.
         cls.connection.close()
 
     def test_get_history(self):
-
+        #Revisa si el endpoint devuelve correctamente lo que esta en la base de datos.
         response = requests.get(self.url)
         self.assertEqual(response.status_code, 200)
         actual_data = response.json()
         self.assertEqual(actual_data, self.expected_data)
 
     def test_get_history_blank(self):
-        
+        #Comprobamos que el sistema no se cae en caso de que la base de datos este vacía.
+        #*Solo va a dar respuesta positiva cuando la base de datos esta vacía.
         response = requests.get(self.url) 
         self.assertEqual(response.status_code, 200)
         actual_data = response.json()
@@ -38,9 +41,11 @@ class HistoryTests(unittest.TestCase):
 
 
 class SearchTests(unittest.TestCase):
+    #Clase de pruebas para el endpoint /busqueda
 
     @classmethod
     def setUpClass(cls) -> None:
+        #Guardamos el estado de la base de datos antes de hacer las pruebas. Además seteamos algunos datos acá para mantener orden.
         cls.url = "http://localhost:3000/busqueda"
         cls.conn = sqlite3.connect('talleres.db')
         cls.cursor = cls.conn.cursor()
@@ -51,6 +56,7 @@ class SearchTests(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
+        #Devolvemos la base de datos al estado que estaba antes de hacer las pruebas y cerramos conexión.
         del cls.url
         del cls.payload_st
         del cls.payload_yoga
@@ -62,18 +68,17 @@ class SearchTests(unittest.TestCase):
 
 
     def test_post_busqueda_notem(self):
+        #Si no se específica la temática el sistema debe informar al usuario que no entrego suficiente información.
         response = requests.post(self.url, json=self.payload_st)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.json() == "ChatGPT no encontro la tematica del taller." or response.json() == "ChatGPT no siguio el formato establecido.")
     
     def test_post_busqueda(self):
+        #Búsqueda válida, se verifica que siga el formato que se solicita (Una lista de diccionarios).
         response = requests.post(self.url, json=self.payload_yoga)
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
         self.assertIsInstance(response.json()[1], dict)
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
